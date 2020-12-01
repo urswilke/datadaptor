2c0a50936df0214d6e4a796f6abc251580288162
Urs Wilke <urs.wilke@gmail.com>
2020-12-01 22:49:50 +0100

    typo

--- R/fake_survey.R
+++ R/fake_survey.R
@@ -3,7 +3,7 @@
 #' A dataset containing made-up answers to a survey.
 #' The data is stored in variables of the type haven::labelled.
 #' The same data is also included in the package in SPSS format.
-#' See in  load the SPSS version with
+#' See in the examples section how to load the SPSS version to R.
 #'
 #'
 #' @format A data frame of 100 respondents to 5 questions (variables):
