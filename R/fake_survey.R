929e87e41b88420851831632f92a087593387407
Urs Wilke <urs.wilke@gmail.com>
2020-12-01 22:20:15 +0100

    - add package internal dataset fake_survey - add spss file fake_survey.sav in package - generated by devel/make_fake_data - documentation generated by R/fake_survey.sav

---
+++ R/fake_survey.R
@@ -0,0 +1,21 @@
+#' Fake survey data
+#'
+#' A dataset containing made-up answers to a survey.
+#' The data is stored in variables of the type haven::labelled.
+#' The same data is also included in the package in SPSS format.
+#' See in  load the SPSS version with
+#'
+#'
+#' @format A data frame of 100 respondents to 5 questions (variables):
+#' \describe{
+#'   \item{q1}{How much do you like the product?}
+#'   \item{q2}{Do you want to recommend the product?}
+#'   \item{q3}{How likely will you go dancing this weekend?}
+#'   \item{q4}{How much do you like your friends?}
+#'   \item{q5}{How much do you like your best friend}
+#' }
+#' @examples
+#' datenanpassr::fake_survey
+#' path <- system.file("extdata", "fake_survey.sav", package = "datenanpassr")
+#' haven::read_sav(path)
+"fake_survey"
