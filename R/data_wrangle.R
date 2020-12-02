31369eb573ea1cdf65f7e8333282144713b76bf0
Urs Wilke <urs.wilke@gmail.com>
2020-12-02 16:44:54 +0100

    create first running version of mapp_xl_to_data

--- R/data_wrangle.R
+++ R/data_wrangle.R
@@ -7,8 +7,8 @@
 #' @export
 #'
 #' @examples
-#' map_create(fake_survey, "mapping.xlsx")
-map_create <- function(df_raw, filename) {
+#' mapp_create(fake_survey, "mapping.xlsx")
+mapp_create <- function(df_raw, filename) {
 
   df_varlab <-
     tablab::tab_varlabs(df_raw) %>%
@@ -45,13 +45,14 @@ map_create <- function(df_raw, filename) {
 #' @export
 #'
 #' @examples
-#' map_create(fake_survey, "mapping.xlsx")
-#' map_varl("mapping.xlsx")
-map_varl <- function(filename) {
+#' mapp_create(fake_survey, "mapping.xlsx")
+#' mapp_varl("mapping.xlsx")
+mapp_varl <- function(filename) {
   readxl::read_xlsx(
     filename,
     sheet = "Variables"
-  )
+  ) %>%
+    dplyr::mutate(row = dplyr::row_number() + 1)
 }
 
 
@@ -63,13 +64,14 @@ map_varl <- function(filename) {
 #' @export
 #'
 #' @examples
-#' map_create(fake_survey, "mapping.xlsx")
-#' map_vall("mapping.xlsx")
-map_vall <- function(filename) {
+#' mapp_create(fake_survey, "mapping.xlsx")
+#' mapp_vall("mapping.xlsx")
+mapp_vall <- function(filename) {
   readxl::read_xlsx(
     filename,
     sheet = "Labels"
-  )
+  ) %>%
+    dplyr::mutate(row = dplyr::row_number() + 1)
 }
 
 
@@ -81,17 +83,26 @@ map_vall <- function(filename) {
 #' @export
 #'
 #' @examples
-#' map_create(fake_survey, "mapping.xlsx")
-#' map_free1("mapping.xlsx")
-map_free1 <- function(filename) {
-  readxl::read_xlsx(
+#' mapp_create(fake_survey, "mapping.xlsx")
+#' mapp_free1("mapping.xlsx")
+mapp_free1 <- function(filename) {
+  res <- readxl::read_xlsx(
     filename,
-    sheet = "Free1"
-  ) %>%
-    dplyr::select(1:5) %>%
-    dplyr::rename_all( ~ paste0("X", 1:5)) %>%
-    dplyr::mutate(row = row_number()) %>%
-    dplyr::filter_at(-6, any_vars(!is.na(.)))
+    sheet = "Free1",
+    range = cellranger::cell_cols("A:E"),
+    col_names = FALSE,
+    col_types = "text"
+  )
+  if (nrow(res) > 0) {
+    res %>%
+      dplyr::select(1:5) %>%
+      dplyr::rename_all( ~ paste0("X", 1:5)) %>%
+      dplyr::filter_all(dplyr::any_vars(!is.na(.))) %>%
+      dplyr::mutate(row = dplyr::row_number())
+  }
+  else {
+    purrr::map_dfc(1:5, ~character()) %>% purrr::set_names(paste0("X", 1:5))
+  }
 }
 
 
@@ -112,9 +123,9 @@ read_sav_or_dta <- function(filename) {
 tab_n_distinct <- function(df_raw) {
   df_raw %>%
     # remove attributes to prevent warning when gathering:
-    mutate_all(as.vector) %>%
+    dplyr::mutate_all(as.vector) %>%
     gather(var, val, factor_key = TRUE) %>%
-    group_by(var) %>%
+    dplyr::group_by(var) %>%
     summarise(n = n_distinct(val))
 }
 
@@ -126,11 +137,22 @@ create_cnt_compare <- function(l, id){
 
 }
 
-mod_varlabs <- function(df_raw, df_varl) {
-  df_raw[df_varl %>% drop_na(new_label) %>% pull(var)] <-
-    df_raw[df_varl %>% drop_na(new_label) %>% pull(var)] %>%
+#' Modify variable labels according to sheet in Excel mapping file
+#'
+#' @param df_raw labelled dataframe
+#' @param filename name of the Excel mapping file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' mapp_mod_varl(fake_survey, "mapping.xlsx")
+mapp_mod_varl <- function(df_raw, filename) {
+  df_varl <- mapp_varl(filename)
+  df_raw[df_varl %>% tidyr::drop_na(new_label) %>% dplyr::pull(var)] <-
+    df_raw[df_varl %>% tidyr::drop_na(new_label) %>% dplyr::pull(var)] %>%
     map2_dfc(.,
-             df_varl %>% drop_na(new_label) %>% pull(new_label),
+             df_varl %>% tidyr::drop_na(new_label) %>% dplyr::pull(new_label),
              ~ {
                attr(.x, "label") <- .y
                .x
@@ -161,7 +183,7 @@ add_labs <- function(df_raw, data){
   x <- df_raw %>% pull(!!data$X2[1])
   labs <- c(attr(x, "labels") %>% names(), data$X3[-1])
   vals <- c(attr(x, "labels") %>% unname(), data$X2[-1] %>% as.numeric())
-  attr(x, "label") <- coalesce(data$X3[1], attr(x, "label"))
+  attr(x, "label") <- dplyr::coalesce(data$X3[1], attr(x, "label"))
   attr(x, "labels") <- setNames(vals, labs)
   df_raw %>%
     mutate(!!data$X2[1] := x)
@@ -171,78 +193,78 @@ add_labs <- function(df_raw, data){
 kg <- function(df_raw, var1, var2) {
   var_kg <- paste(var1, var2, sep = "_")
   df_raw %>%
-    mutate(!!var_kg := fct_cross(!!sym(var1) %>% as_factor(), !!sym(var2) %>% as_factor()))
+    mutate(!!var_kg := fct_cross(!!rlang::sym(var1) %>% as_factor(), !!rlang::sym(var2) %>% as_factor()))
 
 }
 
 rec_1var <- function(l_sum_var_el, df_raw) {
-  var_name <- l_sum_var_el %>% pull(var) %>% .[1]
+  var_name <- l_sum_var_el %>% dplyr::pull(var) %>% .[1]
   sum_var_name <- var_name %>% paste0("k", .)
-  sum_var_label <- l_sum_var_el %>% pull(`sum var label`) %>% .[1]
+  sum_var_label <- l_sum_var_el %>% dplyr::pull(sum_var_label) %>% .[1]
 
   sum_var_vals_n_labs <- l_sum_var_el %>%
-    group_by(`sum var value`) %>%
-    summarise(val_lists = list(value),
-              val_labs = first(`sum var vallab`))
+    dplyr::group_by(sum_var_value) %>%
+    dplyr::summarise(val_lists = list(nv),
+              val_labs = dplyr::first(sum_var_vallab))
 
-  cond_statements <- map2(sum_var_vals_n_labs$val_lists,
-                          sum_var_vals_n_labs$`sum var value`,
-                          ~ quo(!!sym(var_name) %in% !!.x ~ !!.y)
+  cond_statements <- purrr::map2(sum_var_vals_n_labs$val_lists,
+                          sum_var_vals_n_labs$sum_var_value,
+                          ~ rlang::quo(!!rlang::sym(var_name) %in% !!.x ~ !!.y)
   )
 
 
 
-  df_raw <- df_raw %>% mutate(sum_var := case_when(!!!cond_statements))
+  df_raw <- df_raw %>% dplyr::mutate(sum_var := dplyr::case_when(!!!cond_statements))
   attr(df_raw$sum_var, "label") <- sum_var_label
-  attr(df_raw$sum_var, "labels") <- sum_var_vals_n_labs[-2] %>% deframe()
-  df_raw %>% rename(!!sym(sum_var_name) := sum_var)
+  attr(df_raw$sum_var, "labels") <- sum_var_vals_n_labs[-2] %>% tibble::deframe()
+  df_raw %>% dplyr::rename(!!rlang::sym(sum_var_name) := sum_var)
 }
 
 
 rec_1var_free <- function(l_sum_var_el, df_raw) {
-  var_name <- l_sum_var_el %>% pull(X2) %>% .[1]
-  sum_var_name <- l_sum_var_el %>% pull(X3) %>% .[1]
-  sum_var_label <- l_sum_var_el %>% pull(X5) %>% .[1]
+  var_name <- l_sum_var_el %>% dplyr::pull(X2) %>% .[1]
+  sum_var_name <- l_sum_var_el %>% dplyr::pull(X3) %>% .[1]
+  sum_var_label <- l_sum_var_el %>% dplyr::pull(X5) %>% .[1]
 
-  sum_var_vals_n_labs <- l_sum_var_el %>% slice(-1)
+  sum_var_vals_n_labs <- l_sum_var_el %>% dplyr::slice(-1)
 
   rec_vecs <-
     l_sum_var_el %>%
-    slice(-1) %>%
-    mutate_all(as.numeric) %>%
-    select(X2, X3, X4) %>%
+    dplyr::slice(-1) %>%
+    dplyr::mutate_all(as.numeric) %>%
+    dplyr::select(X2, X3, X4) %>%
     as.list() %>%
     unname()
 
   cond_statements <-
-    pmap(rec_vecs,
-         function(x,y,z) quo(!!sym(var_name) >= !!x & !!sym(var_name) <= coalesce(!!y, !!x)  ~ !!z)
+    purrr::pmap(rec_vecs,
+         function(x,y,z) rlang::quo(!!rlang::sym(var_name) >= !!x & !!rlang::sym(var_name) <= dplyr::coalesce(!!y, !!x)  ~ !!z)
     )
 
 
 
 
-  df_raw <- df_raw %>% mutate(sum_var := case_when(!!!cond_statements))
+  df_raw <- df_raw %>% dplyr::mutate(sum_var := dplyr::case_when(!!!cond_statements))
   attr(df_raw$sum_var, "label") <- sum_var_label
-  attr(df_raw$sum_var, "labels") <- sum_var_vals_n_labs[-2] %>% deframe()
-  df_raw %>% rename(!!sym(sum_var_name) := sum_var)
+  attr(df_raw$sum_var, "labels") <- sum_var_vals_n_labs[-2] %>% tibble::deframe()
+  df_raw %>% dplyr::rename(!!rlang::sym(sum_var_name) := sum_var)
 }
 
 
 extract_sev_lists <- function(var) {
   l_sev_parts <-
     var %>%
-    str_squish() %>%
-    str_extract_all("(\\{.+?\\})", simplify = T) %>%
+    stringr::str_squish() %>%
+    stringr::str_extract_all("(\\{.+?\\})", simplify = T) %>%
     # as_tibble() %>%
-    map(~str_remove_all(.x, "[\\{\\}]")) %>%
-    str_split(" ", simplify = T) %>%
-    as_tibble()
+    purrr::map(~stringr::str_remove_all(.x, "[\\{\\}]")) %>%
+    stringr::str_split(" ", simplify = T) %>%
+    tibble::as_tibble()
 
   replace_1curly <- function(orig_str, replacement) str_replace(orig_str,  "\\{.+?\\}", replacement)
-  replace_all_curlies <- function(orig_str, l_1sev_parts) reduce(l_1sev_parts, replace_1curly, .init = orig_str)
+  replace_all_curlies <- function(orig_str, l_1sev_parts) purrr::reduce(l_1sev_parts, replace_1curly, .init = orig_str)
   if (!all(dim(l_sev_parts) == c(0,0))) {
-    l_sev_parts %>% map_chr(~replace_all_curlies(var, .x)) %>% unname()
+    l_sev_parts %>% purrr::map_chr(~replace_all_curlies(var, .x)) %>% unname()
   }
   else {
     var
@@ -251,8 +273,8 @@ extract_sev_lists <- function(var) {
 
 severalize <- function(df_f1) {
   df_if_or_comp <-
-    df_f1[str_detect(df_f1$X1, "(^#IF|^#COMP)"),] %>% filter_all(any_vars(!is.na(.)))  %>%
-    mutate_at(2:3, ~map(.x,~extract_sev_lists(.))) %>% unnest(cols = c("X2", "X3"))
+    df_f1[stringr::str_detect(df_f1$X1, "(^#IF|^#COMP)"),] %>% dplyr::filter_all(dplyr::any_vars(!is.na(.)))  %>%
+    dplyr::mutate_at(2:3, ~purrr::map(.x,~extract_sev_lists(.))) %>% tidyr::unnest(cols = c("X2", "X3"))
   df_if_or_comp
   # df_f1_mod <-
   #   df_f1
@@ -271,15 +293,15 @@ severalize <- function(df_f1) {
 mutate_comp <- function(df_raw, var_str, expr_str) {
   expi <- rlang::parse_expr(expr_str)
 
-  df_raw %>% mutate(!!var_str := !!expi)
+  df_raw %>% dplyr::mutate(!!var_str := !!expi)
 }
 
 
 mutate_cond <- function(df_raw, cond_str, assign_str) {
   condi <- rlang::parse_expr(cond_str)
-  assi <- assign_str %>% str_split("=") %>% unlist() %>%  rlang::parse_exprs()
+  assi <- assign_str %>% stringr::str_split("=") %>% unlist() %>%  rlang::parse_exprs()
 
-  df_raw %>% mutate(!!assi[[1]] := ifelse(!!condi, !!assi[[2]], NA_real_))
+  df_raw %>% dplyr::mutate(!!assi[[1]] := ifelse(!!condi, !!assi[[2]], NA_real_))
 }
 
 
@@ -294,57 +316,79 @@ diff_daff <- function(df_raw, df_mod) {
 
 create_df_sumvar <- function(df_vall) {
   df_vall %>%
-    drop_na(`sum var value`) %>%
-    select(-`New label`) %>%
-    mutate(new_var = paste0("k", var)) %>%
-    mutate(orig_var = var) %>%
-    group_by(new_var, orig_var) %>%
-    mutate(row = paste(row, collapse = ", ")) %>%
-    mutate(sheet = "Labels") %>%
-    mutate(action = "#SUMVAR") %>%
-    select(sheet, action, everything())
+    tidyr::drop_na(sum_var_value) %>%
+    dplyr::select(-new_label) %>%
+    dplyr::mutate(new_var = paste0("k", var)) %>%
+    dplyr::mutate(orig_var = var) %>%
+    dplyr::group_by(new_var, orig_var) %>%
+    dplyr::mutate(row = paste(row, collapse = ", ")) %>%
+    dplyr::mutate(sheet = "Labels") %>%
+    dplyr::mutate(action = "#SUMVAR") %>%
+    dplyr::relocate(sheet, action)
 }
 
 create_df_new_lab <- function(df_varl) {
   df_varl %>%
-    mutate(row = (row_number() + 1) %>% as.character()) %>%
-    drop_na(new_label) %>%
-    mutate(new_var = var) %>%
-    mutate(sheet = "Variables") %>%
-    mutate(action = "#NEWLAB")
+    dplyr::mutate(row = (dplyr::row_number() + 1) %>% as.character()) %>%
+    tidyr::drop_na(new_label) %>%
+    dplyr::mutate(new_var = var) %>%
+    dplyr::mutate(sheet = "Variables") %>%
+    dplyr::mutate(action = "#NEWLAB")
 }
 
 
 make_free_table <- function(df_f1) {
-  df_f1 %>%
-    mutate(index = cumsum(coalesce(str_detect(X1, "^#"), FALSE))) %>%
-    group_by(index) %>%
-    mutate(row = paste(row, collapse = ", ")) %>%
-    mutate(action = X1[1]) %>%
-    ungroup() %>%
-    mutate(sheet = "Free1") %>%
-    select(-index) %>%
+  res <- df_f1 %>%
+    dplyr::mutate(index = cumsum(dplyr::coalesce(stringr::str_detect(X1, "^#"), FALSE))) %>%
+    dplyr::group_by(index) %>%
+    dplyr::mutate(row = paste(row, collapse = ", ")) %>%
+    dplyr::mutate(action = X1[1]) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(sheet = "Free1") %>%
+    dplyr::select(-index) %>%
     severalize() %>%
-    group_by(action, row) %>%
-    mutate(new_var = case_when(action == "#REC" ~ X3[1],
-                               action == "#IF" ~ str_remove(X3, "=.*") %>% str_squish(),
-                               action == "#COMP" ~ X2,
-                               action == "#VARL" ~ X2,
-                               action == "#KG" ~ paste(X2, X3, sep = "_"),
-                               action %in% c("#VALL", "#AVALL") ~ X2[1]))
+    dplyr::group_by(action, row)
+  if (nrow(res) > 0) {
+    res %>%
+      dplyr::mutate(
+        new_var = dplyr::case_when(
+          action == "#REC" ~ X3[1],
+          action == "#IF" ~ stringr::str_remove(X3, "=.*") %>% stringr::str_squish(),
+          action == "#COMP" ~ X2,
+          action == "#VARL" ~ X2,
+          action == "#KG" ~ paste(X2, X3, sep = "_"),
+          action %in% c("#VALL", "#AVALL") ~ X2[1]
+        )
+      )
+  }
+  else {
+    tibble::tibble()
+  }
 }
 
-create_mod_table <- function(df_varl, df_vall, df_f1) {
-  df_vall <-
-    df_vall %>%
-    mutate(row = row_number() + 1)
+#' Create a summary table of the data modifications in the
+#' Excel mapping file
+#'
+#' @param filename filename of the Excel mapping file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' mapp_create(fake_survey, "mapping.xlsx")
+#' mapp_mod_table("mapping.xlsx")
+mapp_mod_table <- function(filename) {
+  df_varl <- mapp_varl(filename)
+  df_vall <- mapp_vall(filename)
+  df_free1 <- mapp_free1(filename)
 
-  df_f1_commands <- make_free_table(df_f1 %>% severalize())
+
+  df_f1_commands <- make_free_table(df_free1 %>% severalize())
   create_df_new_lab(df_varl) %>%
-    bind_rows(create_df_sumvar(df_vall)) %>%
-    bind_rows(df_f1_commands)  %>%
-    group_by(sheet, action, row, new_var) %>%
-    nest()
+    dplyr::bind_rows(create_df_sumvar(df_vall)) %>%
+    dplyr::bind_rows(df_f1_commands)  %>%
+    dplyr::group_by(sheet, action, row, new_var) %>%
+    tidyr::nest()
 }
 
 
@@ -363,8 +407,17 @@ all_mutes <- function(df_raw, action, data) {
   )
 }
 
-reduce_da <- function(df_raw, df_varl, df_vall, df_f1) {
-  mod_table <- create_mod_table(df_varl, df_vall, df_f1)
+#' Apply changes of mapping Excel file to dataframe
+#'
+#' @param df_raw dataframe to apply mapping on
+#' @param filename name of the Excel file with mappings
+#'
+#' @return
+#' @export
+#'
+#' @examples
+mapp_xl_to_data <- function(df_raw, filename) {
+  mod_table <- mapp_mod_table(filename)
 
-  reduce2(mod_table$action, mod_table$data, all_mutes, .init = df_raw)
+  purrr::reduce2(mod_table$action, mod_table$data, all_mutes, .init = df_raw)
 }
