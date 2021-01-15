3b7a9fad909975124398e89346f18ea2c51bcbe7
Urs Wilke <urs.wilke@gmail.com>
2021-01-15 16:48:38 +0100

    copy make_verbatim_cmd_table() code to standalone script

---
+++ devel/verbatim_cmd_tbl_script.R
@@ -0,0 +1,166 @@
+library(tidyverse)
+
+map_file <- "inst/extdata/mapping.xlsx"
+verba_file = datenanpassr:::mapp_extract_verbatim_file(map_file, "Verbatims")
+sheet = "Verbatims"
+
+if (is.na(verba_file)) {
+  return(tibble::tibble())
+}
+verba_file_sheets <-
+  verba_file %>%
+  readxl::excel_sheets() %>%
+  # except "Codestufen", the first sheet:
+  .[-1] %>%
+  # by setting names, the map function applied later, will assign these names to
+  # the created list elements...
+  purrr::set_names()
+
+read_assigns <- function(sheet_name){
+  readxl::read_excel(verba_file, sheet = sheet_name, col_names = TRUE, range = cellranger::cell_limits(ul = c(32, 4))) %>%
+    dplyr::select(`Orig. Variable`, ID, Antwort, matches("^Zuord "))
+}
+
+
+# gather & prepare all assignments ------------------------------------------
+l_assigns_raw <-
+  verba_file_sheets %>%
+  purrr::map(~read_assigns(.x))
+
+
+mapping_verba_sheet <-
+  readxl::read_excel(map_file,
+                     skip = 14,
+                     sheet = sheet,
+                     col_names = TRUE) %>%
+  tidyr::drop_na(VariableOriginal) %>%
+  dplyr::select(VariableOriginal:`Tabellen-blatt`, VariableZiel)
+
+
+
+
+
+# * gather all assignments in a long data frame ------------------------------------------
+
+#function to replace the term {OT...} in var_ziel by the corresponding substring
+#in orig_var:
+un_OT_ize <- function(var_ziel,orig_var){
+  # exctract the three digits in {OT...} :
+  copyDigits <- stringr::str_match(var_ziel,"\\{OT(.*?)\\}")[,2]
+  # the first two digits in the beginning represent the starting positition:
+  cp1stPos <- copyDigits %>% stringr::str_match("^\\d\\d") %>% as.numeric()
+  # the last digit in the end represent the length:
+  cpLength <- copyDigits %>% stringr::str_match("\\d$") %>% as.numeric()
+  # extract substring of orig_var:
+  replaceStr <- stringr::str_sub(orig_var, cp1stPos, cp1stPos + cpLength - 1)
+  # replace the term {OT...} by the latter substring:
+  var_name <- stringr::str_replace(var_ziel,"\\{OT\\d\\d\\d\\}",replaceStr)
+  var_name
+}
+
+df_assigns <-
+  l_assigns_raw %>%
+  # write all the assignments in the same data frame:
+  dplyr::bind_rows(.id = "q_id") %>%
+  # add the EFA1MCG2MDG3 information & VariableZiel from the verbatim sheet in
+  # the mapping file:
+  dplyr::full_join(., mapping_verba_sheet, by = c("Orig. Variable" = "VariableOriginal")) %>%
+  # gather all the assignments in two columns: i_assign is the index of the
+  # column (Zuord 1, ..., 5) and code_assign is the assigned code:
+  tidyr::gather(i_assign, code_assign, starts_with("Zuord")) %>%
+  # this removes the data where nothing was assigned. If one wants to have a
+  # stable number of variables assigned, this should be changed:
+  tidyr::drop_na(code_assign) %>%
+  # remove the substring "Zuord ":
+  dplyr::mutate(i_assign = stringr::str_remove(i_assign, "^Zuord ")) %>%
+  # replace the {OT...} term:
+  dplyr::mutate(VariableZiel = un_OT_ize(VariableZiel, `Orig. Variable`)) %>%
+  # depending on the variable type, the name of the variable to be assigned is
+  # created by replacing the "{nn}" term: if one wants to allow multiple
+  # assigned variable for EFA, the first line has to be adapted according to the
+  # second...
+  dplyr::mutate(var_ziel = dplyr::case_when(EFA1MCG2MDG3 == 1 ~ stringr::str_remove(VariableZiel, "\\{nn\\}"),
+                                            EFA1MCG2MDG3 == 2 ~ stringr::str_replace(VariableZiel, "\\{nn\\}", as.character(i_assign)),
+                                            EFA1MCG2MDG3 == 3 ~ stringr::str_replace(VariableZiel, "\\{nn\\}", as.character(code_assign)))) %>%
+  # if there are several assignment columns for EFA variables, this line removes
+  # them:
+  # filter(!(EFA1MCG2MDG3 == 1 & i_assign != 1)) %>%
+  # the value to be assigned for mdg variables is 1, otherwise code_assign:
+  dplyr::mutate(val_assign = dplyr::if_else(EFA1MCG2MDG3 == 3, 1, code_assign)) %>%
+  # the column code_assign is only needed for mdg variables to assign the right
+  # variable labels later. Therefore, it is set to NA for the other variables:
+  # dplyr::mutate(code_assign = dplyr::if_else(EFA1MCG2MDG3 != 3, NA_real_, code_assign)) %>%
+  dplyr::select(-i_assign) %>%
+  dplyr::rename(DC_ID = ID) %>%
+  # the sorting of DC_ID is probably very important that the assigning of the
+  # verbatim codes later is done in the correct order:
+  dplyr::arrange(DC_ID)
+
+df_codestufen <-
+  readxl::read_excel(
+    verba_file,
+    sheet = "Codestufen",
+    col_names = TRUE,
+    range = cellranger::cell_limits(ul = c(1, 2))
+  ) %>%
+  # in the Codestufen sheet there is no title for the code column; R
+  # automatically has given the name X__1; change it to "Code"...:
+  dplyr::mutate(Code = dplyr::row_number()) %>%
+  tidyr::gather(q_id, Beschreibung,-Code) %>%
+  # only retain codes where a category ("Beschreibung") exists:
+  tidyr::drop_na(Beschreibung)
+
+df_cats <-
+  df_assigns %>%
+  # this extracts all the variables that are assigned var_ziel (the other
+  # columns don't add cases...):
+  dplyr::distinct(q_id, var_ziel, code_assign, EFA1MCG2MDG3) %>%
+  # the labelled values are joint. Every variable var_ziel will be repeated the
+  # number of times, there are different codes in the corresponding sheet in the
+  # verbatim file:
+  dplyr::left_join(
+    .,
+    df_codestufen,
+    by = c("q_id", "code_assign" = "Code")
+  ) %>%
+  # for mdg types, the only line with the correct variable label is the one
+  # where the assigned code code_assign is equal to the Code in the Codestufen
+  # sheet:
+  # dplyr::filter(EFA1MCG2MDG3 != 3 | code_assign == Code) %>%
+  # for mdg variables, a variable label column is created, for the other types
+  # the variable label is set to the empty string:
+  dplyr::mutate(varlab=dplyr::case_when(EFA1MCG2MDG3 == 3 ~ Beschreibung,
+                                        EFA1MCG2MDG3 != 3 ~ "")) %>%
+  # create a column containing named vectors containing the values & the value
+  # labels for each var_ziel:
+  dplyr::group_by(q_id, var_ziel) %>%
+  dplyr::summarise(vallabs = list(purrr::set_names(code_assign, Beschreibung)),
+                   varlab  = varlab[1],
+                   EFA1MCG2MDG3 = EFA1MCG2MDG3[1]) %>%
+  # for mdg variables the value labels that where created make no sense and are
+  # not needed (in the future one could add something like "1 = selected"...):
+  dplyr::mutate(vallab = dplyr::if_else(EFA1MCG2MDG3 == 3, list(NULL), vallabs))
+
+
+df_assigns_overview <-
+  df_assigns %>%
+  dplyr::left_join(df_cats, by = c("EFA1MCG2MDG3", "var_ziel")) %>%
+  dplyr::group_by(var_ziel, val_assign, vallab, varlab) %>%
+  dplyr::summarise(
+    id_list = list(as.numeric(DC_ID)),
+    # old:
+    assign_cond =
+      DC_ID %>%
+      paste(., collapse = ", ") %>%
+      paste0("DC_ID %in% c(", .) %>% paste0(")"))
+res <- df_assigns_overview %>%
+  dplyr::mutate(
+    sheet = "verbatims",
+    new_var = var_ziel,
+    action = "#Verba",
+    row = dplyr::cur_group_id() %>% as.character()
+  ) %>%
+  dplyr::ungroup()  %>%
+  dplyr::group_by(sheet, action, row, new_var) %>%
+  tidyr::nest()
+res
