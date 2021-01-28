cb796b4ec051bcae6731e7da3a3cca07db312907
Urs Wilke <urs.wilke@gmail.com>
2021-01-28 17:36:04 +0100

    add test that makes sure that translate_to_r_script results in the same as mapp_xl_to_data

--- tests/testthat/test-mapping_functions.R
+++ tests/testthat/test-mapping_functions.R
@@ -1,15 +1,26 @@
 spss_filepath <- system.file("extdata", "fake_survey.sav", package = "datenanpassr")
-df <- haven::read_sav(spss_filepath)
+# don't call this dataframe df, as it would mask the dataframe created when
+# source()ing the temporary script "mapping.R":
+df_test <- haven::read_sav(spss_filepath)
 mapping_filepath <- system.file("extdata", "mapping.xlsx", package = "datenanpassr")
+df_cmd <- mapp_cmd_table(mapping_filepath)
+df_mod <- mapp_xl_to_data(df_test, df_cmd)
 
 test_that("mapping function reproduces snapshot", {
-  df_mod <- mapp_xl_to_data(df, mapping_filepath)
   testthat::expect_snapshot_output(df_mod %>% str())
 })
 
 
 test_that("mapp_cmd_table() reproduces snapshot", {
-  df_cmd <- mapp_cmd_table(mapping_filepath)
   testthat::expect_snapshot_output(df_cmd %>% str())
+})
+
+test_that("translate_to_r_script results in the same as mapp_xl_to_data", {
+  withr::with_file("mapping.R", {
+    df_mod <- mapp_xl_to_data(df_test, df_cmd, na_to_filter = FALSE)
+    translate_to_r_script(df_cmd, rscript_name = "mapping.R", spss_filepath)
+    source("mapping.R", echo = FALSE)
+    testthat::expect_equal(df_mod, df)
+  })
 })
 
