2248ccdd3d96da55604b1f2755cfa59ad953b514
Urs Wilke <urs.wilke@gmail.com>
2021-03-13 10:55:17 +0100

    clean up

--- devel/run_new_cmd_vectorized_on_subset.R
+++ devel/run_new_cmd_vectorized_on_subset.R
@@ -5,18 +5,8 @@ df <- haven::read_sav(spss_file)
 
 mapping_file <- system.file("extdata", "mapping.xlsx", package = "datenanpassr")
 df_cmd <- datenanpassr::mapp_cmd_table(mapping_file, vars_as_syms = TRUE)
+df_cmd_old <- datenanpassr::mapp_cmd_table(mapping_file)
 
-args_contain_renamed <- df_cmd %>%
-  select(data) %>%
-  unnest_wider(data) %>%
-  select(where(is.character)) %>%
-  mutate(a = rowSums(map_dfc(., ~str_detect(.x, "renamed")), na.rm = T)) %>%
-  pull(a)
-
-df_cmd_subset <- df_cmd %>%
-  filter(args_contain_renamed == 0) %>%
-  filter(!action %in% c("#RECNA", "#RENAME", "#RFUN")) %>%
-  filter(!str_detect(new_var, "renamed"))
 
 generate_cmd_expression_vec <- function(action, data) {
   switch (
@@ -55,57 +45,61 @@ get_mutate_exprs <- function(action, data, new_var) {
   names(cmds)[action %in% c("#MERGE", "#KG", "#R")] <- ""
   cmds
 }
-cmds <- get_mutate_exprs(df_cmd_subset$action, df_cmd_subset$data, df_cmd_subset$new_var)
-df_mod <- df %>% mutate(!!!cmds)
-df %>% mutate(!!cmds[[55]])
-
-df_cmd_old <- datenanpassr::mapp_cmd_table(mapping_file)
-df_cmd_subset_old <- df_cmd_old %>%
-  filter(args_contain_renamed == 0) %>%
-  filter(!action %in% c("#RECNA", "#RENAME", "#RFUN")) %>%
-  filter(!str_detect(new_var, "renamed"))
-df_mod_old <- mapp_xl_to_data(df, df_cmd_subset_old)
-all.equal(df_mod, df_mod_old)
 
 
-
-# separate mutate / reduce blocks -----------------------------------------
+# separate mutate / reduce groups -----------------------------------------
 
 split_df_cmd <- function(df_cmd) {
+  single_groups <- c("#RECNA", "#RENAME", "#RFUN")
+  prepare_groups <- function(df_cmd_group) {
+    if (!df_cmd_group$action[1] %in% single_groups) {
+      df_cmd_group <- tibble::tibble(
+        action = "#GROUP",
+        data = list(df_cmd_group$cmd)
+      )
+    }
+    df_cmd_group %>%
+      select(action, data)
+  }
+
   df_cmd %>%
     dplyr::mutate(
-      i_block = action %in% c("#RECNA", "#RENAME", "#RFUN"),
-      i_block = i_block != lag(i_block, default = TRUE),
-      i_block = cumsum(i_block)
+      i_group = action %in% single_groups,
+      i_group = i_group != lag(i_group, default = TRUE),
+      i_group = cumsum(i_group)
     ) %>%
-    group_split(i_block, .keep = FALSE) %>%
-    map_dfr(prepare_blocks)
-}
-prepare_blocks <- function(df_cmd_block) {
-  single_blocks <- c("#RECNA", "#RENAME", "#RFUN")
-  if (!df_cmd_block$action[1] %in% single_blocks) {
-    df_cmd_block <- tibble::tibble(
-      action = "#BLOCK",
-      data = list(df_cmd_block$cmd)
-    )
-  }
-  df_cmd_block
+    group_split(i_group, .keep = FALSE) %>%
+    map_dfr(prepare_groups)
 }
 
-df_cmd <- df_cmd %>% mutate(cmd = get_mutate_exprs(action, data, new_var))
-l_cmd <- split_df_cmd(df_cmd)
 
 
-apply_block_manips <- function(df, action, data) {
-  block_expr <- switch (
+generate_group_expr <- function(action, data) {
+  # Hack to prevent R CMD CHECK note
+  # "no visible binding for global variable ‘df’":
+  df <- NULL
+
+  group_expr <- switch (
     action,
     "#RECNA"  = rlang::expr(set_na_to_filter_except(df, !!!data)),
     "#RFUN"   = rlang::expr(cmd_rfun(df, !!!data)),
     "#RENAME" = rlang::expr(cmd_rename(df, !!!data)),
-    "#BLOCK"  = rlang::expr(df %>% mutate(!!!data))
+    "#GROUP"  = rlang::expr(df %>% mutate(!!!data))
   )
-  rlang::eval_tidy(block_expr)
+  group_expr
 }
-df_mod <- reduce2(l_cmd$action, l_cmd$data, apply_block_manips, .init = df)
+
+apply_one_group_cmd <- function(df, action, data){
+  group_expr <- generate_group_expr(action, data)
+  rlang::eval_tidy(group_expr)
+}
+
+group_vectorizable_cmds <- function(df_cmd) {
+  df_cmd <- df_cmd %>% mutate(cmd = get_mutate_exprs(action, data, new_var))
+  df_cmd_groups <- split_df_cmd(df_cmd)
+  df_cmd_groups
+}
+df_cmd_groups <- group_vectorizable_cmds(df_cmd)
+df_mod <- reduce2(df_cmd_groups$action, df_cmd_groups$data, apply_one_group_cmd, .init = df)
 df_mod_old <- mapp_xl_to_data(df, df_cmd_old)
 all.equal(df_mod, df_mod_old)
