045de2d448f253e9cddb6cb5d72a3e2242a18d6f
Urs Wilke <urs.wilke@gmail.com>
2021-04-29 16:20:38 +0200

    restore tests to check correct error behaviour reagrding try_catch = TRUE with new OOP structure

--- tests/testthat/test-mapping_functions.R
+++ tests/testthat/test-mapping_functions.R
@@ -12,7 +12,7 @@ mapping_file <- system.file("extdata", "mapping.xlsx", package = "datenanpassr")
 # ℹ Use `all_of(.x)` instead of `.x` to silence this message.
 # ℹ See <https://tidyselect.r-lib.org/reference/faq-external-vector.html>.
 # This message is displayed once per session.
-testthat::expect_message(testthat::expect_message(mapping_file_struc <- mapp_cmd_table(mapping_file)))
+testthat::expect_message(testthat::expect_message(mapping <- mapp_cmd_table(mapping_file)))
 testthat::expect_message(df_mod <- mapp_xl_to_data(df_test, mapping_file))
 
 testthat::expect_message(df_mod_vec <- mapp_xl_to_data(df_test, mapping_file, vectorized = TRUE))
@@ -30,7 +30,7 @@ test_that("result of the mapping function mapp_xl_to_data()", {
 
 test_that("result of mapp_cmd_table()", {
   testthat::expect_snapshot_output(
-    mapping_file_struc[["df_cmd"]] %>%
+    mapping[["df_cmd"]] %>%
       # dirty hack to remove absolute path (in order to make the test pass on
       # other systems...):
       dplyr::mutate(data = ifelse(
@@ -71,12 +71,12 @@ test_that("translate_to_r_script results in the same as mapp_xl_to_data", {
 #   expect_equal(result, c(1, 7, 3))
 # })
 
-# TODO: make it work with new OOP structure
-# test_that("mapp_xl_to_data() throws error for erroneous code, and message if try_catch = TRUE", {
-#   mapping_file_struc[["df_cmd"]] <- mapping_file_struc[["df_cmd"]] %>% dplyr::filter(action == "#IF") %>% dplyr::slice(1)
-#   mapping_file_struc[["df_cmd"]]$data[[1]]$condition <- "q1 ==(*%$@ 1 |} q3 == 2"
-#   testthat::expect_error(df_mod <- mapp_xl_to_data(df, mapping_file_struc))
-#   testthat::expect_message(df_mod <- mapp_xl_to_data(df, mapping_file_struc, try_catch = TRUE))
-#   testthat::expect_false(attr(df_mod, "error_list") == "")
-# })
+test_that("mapp_xl_to_data() throws error for erroneous code, and message if try_catch = TRUE", {
+  mapping[["df_cmd"]] <- mapping[["df_cmd"]] %>% dplyr::filter(action == "#IF") %>% dplyr::slice(1)
+  mapping[["df_cmd"]]$data[[1]]$condition <- "q1 ==(*%$@ 1 |} q3 == 2"
+  testthat::expect_error(df_mod <- mapp_xl_to_data(df_test, mapping))
+  mapping$try_catch <- TRUE
+  testthat::expect_message(df_mod <- mapp_xl_to_data(df_test, mapping))
+  testthat::expect_false(attr(df_mod, "error_list") == "")
+})
 
