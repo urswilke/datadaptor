f6b6e7070aedf007ad07b147db07addeb72c9e8c
Urs Wilke <urs.wilke@gmail.com>
2021-07-14 13:11:39 +0200

    add more complete example

--- devel/R6_data_mod__test.R
+++ devel/R6_data_mod__test.R
@@ -89,4 +89,43 @@ walk2(df_cmd$action, df_cmd$data, rr$mod)
 rr$dat
 rr$cmd_index
 rr$error_list
+rr$mod(df_cmd$action[1], df_cmd$data[[1]])
+
+# more complete example ---------------------------------------------------
+apply_one_cmd_unsafe_to_self <- function(self, action, data){
+  self$dat <- datenanpassr:::apply_one_cmd.nonvec_unsafe(self$dat, action, data)
+  invisible(self)
+}
+apply_cmds  <- function(self){
+  walk2(self$df_cmd$action, self$df_cmd$data, ~self$mod(self$dat, .x, .y))
+  invisible(self)
+}#,
+
+datamod <- R6::R6Class(
+  "r6datamod",
+  list(
+    dat = NULL,
+    df_cmd = NULL,
+    cmd_index = 0,
+    error_list = vector("character", nrow(df_cmd)),
+    initialize = function(dat, df_cmd){
+      self$dat <- dat
+      self$df_cmd <- df_cmd
+    },
+    apply_cmds = function(){
+      walk2(self$df_cmd$action, self$df_cmd$data, ~self$one_mod(.x, .y))
+      invisible(self)
+    },
+    one_mod = function(action, data){
+      self <- apply_one_cmd_unsafe_to_self(self, action, data)
+      invisible(self)
+    }
+    # mod = apply_one_cmd_unsafe_to_self(self$df_cmd$action, self$df_cmd$data)
+  )
+)
+a <- datamod$new(datenanpassr::fake_survey, df_cmd)
+a$apply_cmds()
+a$dat
+a$df_cmd
+a$cmd_index
 
