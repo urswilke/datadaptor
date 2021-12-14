ed516f53bb10693d7f33104c7b96ac45f42171a6
Urs Wilke <urs.wilke@gmail.com>
2021-12-14 18:24:16 +0100

    move script part of code to file

--- R/mapping.R
+++ R/mapping.R
@@ -1,10 +1,3 @@
-library(tidyverse)
-library(datenanpassr)
-
-source("devel/apply_one_command_s3.R")
-mapping_file <- system.file("extdata", "mapping.xlsx", package = "datenanpassr")
-spss_file <- system.file("extdata", "fake_survey.sav", package = "datenanpassr")
-
 generate_cmd_expression_r6 <- function(action, data) {
   # Hack to prevent R CMD CHECK note
   # "no visible binding for global variable ‘df’":
@@ -106,19 +99,3 @@ Mapping <- R6::R6Class("apply_mods",
 
                          )
 )
-
-# df_cmd = mapp_cmd_table(mapping_file, vectorized = FALSE)$df_cmd
-df_cmd = Mapping$new(spss_file, mapping_file, vectorized = FALSE)
-df_cmd$data[[46]]$condition <- "q1 ==(*%$@ 1 |} q3 == 2"
-df_cmd$data[[47]]$condition <- "q1 ==(*%$@ 1 |} q3 == 2"
-df_cmd$data[[48]]$condition <- "q1 ==(*%$@ 1 |} q3 == 2"
-
-rr <- ApplyMods$new(datenanpassr::fake_survey, mapping_file, try_catch = TRUE)
-# apply first command in df_cmd:
-rr$apply_one_cmd_r6(df_cmd$action[1], df_cmd$data[[1]])
-rr$mod_all()
-rr$dat_mod
-
-rr <- ApplyMods$new(datenanpassr::fake_survey, df_cmd, vectorized = TRUE)
-rr$mod_all()
-rr$dat_mod %>% attr("error_list")
