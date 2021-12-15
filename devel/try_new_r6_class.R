ad84cfbae0e028341cee40fece0414607fb913c5
Urs Wilke <urs.wilke@gmail.com>
2021-12-15 11:49:48 +0100

    add errors to command table

--- devel/try_new_r6_class.R
+++ devel/try_new_r6_class.R
@@ -8,5 +8,12 @@ spss_file <- system.file("extdata", "fake_survey.sav", package = "datenanpassr")
 
 rr <- Mapping$new(spss_file, mapping_file, try_catch = TRUE)
 rr$calc_command_table()
+
+rr$df_cmd$data[[46]]$condition <- "q1 ==(*%$@ 1 |} q3 == 2"
+rr$df_cmd$data[[47]]$condition <- "q1 ==(*%$@ 1 |} q3 == 2"
+rr$df_cmd$data[[48]]$condition <- "q1 ==(*%$@ 1 |} q3 == 2"
+
 rr$mod_all()
 rr$dat_mod
+
+Mapping$debug("initialize")
