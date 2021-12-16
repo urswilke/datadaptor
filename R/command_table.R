b33cf274b7038d2914a6508fd80733077b0f53bd
Urs Wilke <urs.wilke@gmail.com>
2021-12-16 01:06:14 +0100

    merge file to other file

--- R/command_table.R
+++ R/command_table.R
@@ -88,3 +88,48 @@ generate_sheet_cmd_table <- function(self, sheet_cat, sheet_name) {
 
 }
 
+
+switch_sheets_vars_label <- function(sheets) {
+  var_index <- which(sheets == "Variables")
+  lab_index <- which(sheets == "Label")
+  sheets[var_index] <- "Label"
+  sheets[lab_index] <- "Variables"
+  sheets
+}
+tab_sheet_types <- function(sheets) {
+  sheet_types <- c("^Variables", "^Label", "^Verbatims", "^Free")
+
+  # vector of sheets with names defined by types:
+  sheet_cats <- purrr::map(
+    sheets,
+    ~stringr::str_detect(.x, sheet_types)
+  ) %>%
+    purrr::map(
+      ~ sheet_types[.x] %>%
+        stringr::str_remove("\\^")
+    ) %>%
+    purrr::set_names(sheets)
+  # remove sheets not in sheet types list:
+  sheets <- sheets[purrr::map_int(sheet_cats, length) > 0]
+  sheet_cats <- sheet_cats[purrr::map_int(sheet_cats, length) > 0]
+  sheet_cats %>%
+    purrr::map_chr(~.x) %>%
+    tibble::enframe("sheet", "sheet_type")
+}
+
+
+
+apply_df_cmd_manip <- function(df_cmd_manip_string, df_cmd) {
+  df_cmd <- df_cmd_manip_string %>% rlang::parse_expr() %>% rlang::eval_tidy()
+}
+
+generate_sheet_cmd_table <- function(mapping_file, sheet_cat, sheet_name) {
+  switch (
+    sheet_cat,
+    "Variables" = mapp_var_sheet_cmd_table(mapping_file, sheet = sheet_name),
+    "Label"     = mapp_vallab_sheet_cmd_table(mapping_file, sheet = sheet_name),
+    "Free"      = mapp_free_sheet_cmd_table(mapping_file, sheet = sheet_name),
+    "Verbatims" = mapp_verbatim_sheet_cmd_tbl(mapping_file, sheet = sheet_name)
+  )
+
+}
