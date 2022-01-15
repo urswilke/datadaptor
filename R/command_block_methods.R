d8b56941814813a35d382f528cf227db5865b622
Urs Wilke <urs.wilke@gmail.com>
2022-01-15 20:07:59 +0100

    rename argument of apply_command() from x to cmd

--- R/command_block_methods.R
+++ R/command_block_methods.R
@@ -1,6 +1,6 @@
 #' Apply a command block to the mapping
 #'
-#' @param x command_block
+#' @param cdb command_block
 #'
 #' @param self mapping object
 #'
@@ -24,16 +24,16 @@
 #' # The label of the variable in m$dat_mod was modified:
 #' m$dat$q5
 #' m$dat_mod$q5
-apply_command <- function(x, self) {
+apply_command <- function(cdb, self) {
   UseMethod("apply_command")
 }
 # see https://github.com/r-lib/tidyselect/issues/201#issuecomment-650547846:
 utils::globalVariables("where")
 #' @export
-apply_command.cmd_recna_xcpt <- function(x, self) {
-  recode_na_exceptions <- x$args$recode_na_exceptions
-  replace_val <- x$args$replace_val
-  replace_label <- x$args$replace_label
+apply_command.cmd_recna_xcpt <- function(cdb, self) {
+  recode_na_exceptions <- cdb$args$recode_na_exceptions
+  replace_val <- cdb$args$replace_val
+  replace_label <- cdb$args$replace_label
 
   # remove variable names not found in df:
   # TODO: think of cleaner way to do this:
@@ -48,8 +48,8 @@ apply_command.cmd_recna_xcpt <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_r <- function(x, self) {
-  r_code <- x$args$r_code
+apply_command.cmd_r <- function(cdb, self) {
+  r_code <- cdb$args$r_code
   new_df <- r_code %>%
     rlang::parse_expr() %>%
     eval_in_data(self)
@@ -57,9 +57,9 @@ apply_command.cmd_r <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_rfun <- function(x, self) {
-  r_script <- x$args$r_script
-  fun_name <- x$args$fun_name
+apply_command.cmd_rfun <- function(cdb, self) {
+  r_script <- cdb$args$r_script
+  fun_name <- cdb$args$fun_name
   if (!is.na(r_script)) {
     source(r_script, echo = FALSE)
   }
@@ -68,9 +68,9 @@ apply_command.cmd_rfun <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_kg <- function(x, self) {
-  split_var_name <- x$args$split_var
-  by_var_name <- x$args$by_var
+apply_command.cmd_kg <- function(cdb, self) {
+  split_var_name <- cdb$args$split_var
+  by_var_name <- cdb$args$by_var
 
   split_var <- self$dat_mod[[split_var_name]]
   by_var <- self$dat_mod[[by_var_name]]
@@ -88,20 +88,20 @@ apply_command.cmd_kg <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_drop <- function(x, self) {
-  orig_vars <- x$args$orig_vars
+apply_command.cmd_drop <- function(cdb, self) {
+  orig_vars <- cdb$args$orig_vars
   self$dat_mod[orig_vars] <- NULL
 }
 
 #' @export
-apply_command.cmd_verbatim <- function(x, self) {
-  var_ziel <- x$args$var_ziel
-  val_assign <- x$args$val_assign
-  varlab <- x$args$varlab
-  vallab <- x$args$vallab
+apply_command.cmd_verbatim <- function(cdb, self) {
+  var_ziel <- cdb$args$var_ziel
+  val_assign <- cdb$args$val_assign
+  varlab <- cdb$args$varlab
+  vallab <- cdb$args$vallab
   id <- self$params$id_var
-  id_list <- x$args$id_list
-  init_val <- x$args$init_val
+  id_list <- cdb$args$id_list
+  init_val <- cdb$args$init_val
 
   if (!var_ziel %in% names(self$dat_mod)) {
     self$dat_mod[[var_ziel]] <- init_val
@@ -121,10 +121,10 @@ apply_command.cmd_verbatim <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_merge <- function(x, self) {
-  variable_names <- x$args$variable_names
-  merge_file <- x$args$merge_file
-  id <- x$args$id
+apply_command.cmd_merge <- function(cdb, self) {
+  variable_names <- cdb$args$variable_names
+  merge_file <- cdb$args$merge_file
+  id <- cdb$args$id
 
   merge_vars <- c(id, variable_names)
   df_merge <- haven::read_sav(merge_file)
@@ -154,19 +154,19 @@ apply_command.cmd_merge <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_rename <- function(x, self) {
-  orig_vars <- x$args$orig_vars
-  new_names <- x$args$new_names
+apply_command.cmd_rename <- function(cdb, self) {
+  orig_vars <- cdb$args$orig_vars
+  new_names <- cdb$args$new_names
 
   self$dat_mod <- self$dat_mod %>%
     dplyr::rename(!!!purrr::set_names(orig_vars, new_names))
 }
 
 #' @export
-apply_command.cmd_if <- function(x, self) {
-  new_var <- x$args$new_var
-  condition <- x$args$condition
-  new_val <- x$args$new_val
+apply_command.cmd_if <- function(cdb, self) {
+  new_var <- cdb$args$new_var
+  condition <- cdb$args$condition
+  new_val <- cdb$args$new_val
 
   cond <- rlang::parse_expr(condition)
   val <- rlang::parse_expr(new_val)
@@ -198,9 +198,9 @@ eval_in_data <- function(e, self) {
 
 #' @export
 #' @importFrom rlang `%||%`
-apply_command.cmd_comp <- function(x, self) {
-  new_var <- x$args$new_var
-  new_val <- x$args$new_val
+apply_command.cmd_comp <- function(cdb, self) {
+  new_var <- cdb$args$new_var
+  new_val <- cdb$args$new_val
 
   val <- rlang::parse_expr(new_val)
 
@@ -231,9 +231,9 @@ apply_command.cmd_comp <- function(x, self) {
 apply_command.cmd_compr <- apply_command.cmd_comp
 
 #' @export
-apply_command.cmd_set_lab <- function(x, self) {
-  orig_var <- x$args$orig_var
-  new_lab <- x$args$new_lab
+apply_command.cmd_set_lab <- function(cdb, self) {
+  orig_var <- cdb$args$orig_var
+  new_lab <- cdb$args$new_lab
 
   vec <- self$dat_mod[[orig_var]]
   self$dat_mod[[orig_var]] <- haven::labelled(
@@ -247,11 +247,11 @@ apply_command.cmd_set_lab <- function(x, self) {
 apply_command.cmd_newlab <- apply_command.cmd_set_lab
 
 #' @export
-apply_command.cmd_set_labs <- function(x, self) {
-  orig_var <- x$args$orig_var
-  new_lab <- x$args$new_lab
-  new_vals <- x$args$new_vals
-  new_labs <- x$args$new_labs
+apply_command.cmd_set_labs <- function(cdb, self) {
+  orig_var <- cdb$args$orig_var
+  new_lab <- cdb$args$new_lab
+  new_vals <- cdb$args$new_vals
+  new_labs <- cdb$args$new_labs
 
   if (is.null(new_lab)) {
     new_lab <- attr(self$dat_mod[[orig_var]], "label", exact = TRUE)
@@ -264,11 +264,11 @@ apply_command.cmd_set_labs <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_add_labs <- function(x, self) {
-  orig_var <- x$args$orig_var
-  new_lab <- x$args$new_lab
-  vals_added <- x$args$vals_added
-  labs_added <- x$args$labs_added
+apply_command.cmd_add_labs <- function(cdb, self) {
+  orig_var <- cdb$args$orig_var
+  new_lab <- cdb$args$new_lab
+  vals_added <- cdb$args$vals_added
+  labs_added <- cdb$args$labs_added
 
   vec <- self$dat_mod[[orig_var]]
   old_vallab_vec <- attr(vec, "labels")
@@ -293,22 +293,22 @@ apply_command.cmd_add_labs <- function(x, self) {
 apply_command.cmd_newvall <- apply_command.cmd_add_labs
 
 #' @export
-apply_command.cmd_rec <- function(x, self) {
-  orig_var <- x$args$orig_var
-  new_lab <- x$args$new_lab
+apply_command.cmd_rec <- function(cdb, self) {
+  orig_var <- cdb$args$orig_var
+  new_lab <- cdb$args$new_lab
   if (is.na(new_lab)) {
     new_lab <- attr(self$dat_mod[[orig_var]], "label", exact = TRUE)
   }
 
-  new_var <- x$args$new_var
+  new_var <- cdb$args$new_var
   if (is.na(new_var)) {
     new_var <- orig_var
   }
 
-  lb <- x$args$lb
-  ub <- x$args$ub
-  new_vals <- x$args$new_vals
-  new_labs <- x$args$new_labs
+  lb <- cdb$args$lb
+  ub <- cdb$args$ub
+  new_vals <- cdb$args$new_vals
+  new_labs <- cdb$args$new_labs
   recode_df <-
     tibble::tibble(lb, ub = dplyr::coalesce(ub, lb), new_vals, new_labs) %>%
     dplyr::mutate(
@@ -326,13 +326,13 @@ apply_command.cmd_rec <- function(x, self) {
       function(new_vals, expr_str) rlang::quo(!!rlang::parse_expr(expr_str) ~ !!new_vals)
     )
 
-  x <- rlang::expr(dplyr::case_when(!!!cond_statements)) %>% eval_in_data(self)
+  res_num <- rlang::expr(dplyr::case_when(!!!cond_statements)) %>% eval_in_data(self)
   if(new_var == orig_var) {
-    x <- dplyr::coalesce(x, self$dat_mod[[orig_var]])
+    res_num <- dplyr::coalesce(res_num, self$dat_mod[[orig_var]])
   }
 
   self$dat_mod[[new_var]] <- haven::labelled(
-    x,
+    res_num,
     labels = purrr::set_names(recode_df$new_vals, recode_df$new_labs),
     label = new_lab
   )
@@ -340,13 +340,13 @@ apply_command.cmd_rec <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_sumvar <- function(x, self) {
-  new_var <- x$args$new_var
-  orig_var <- x$args$orig_var
-  new_lab <- x$args$new_lab
-  orig_vals <- x$args$orig_vals
-  new_vals <- x$args$new_vals
-  new_labs <- x$args$new_labs
+apply_command.cmd_sumvar <- function(cdb, self) {
+  new_var <- cdb$args$new_var
+  orig_var <- cdb$args$orig_var
+  new_lab <- cdb$args$new_lab
+  orig_vals <- cdb$args$orig_vals
+  new_vals <- cdb$args$new_vals
+  new_labs <- cdb$args$new_labs
   if (is.null(new_lab)) {
     new_lab <- attr(self$dat_mod[[orig_var]], "label", exact = TRUE)
   }
@@ -363,10 +363,10 @@ apply_command.cmd_sumvar <- function(x, self) {
     ~ rlang::expr(!!rlang::sym(orig_var) %in% !!.x ~ !!.y)
   )
 
-  x <- rlang::expr(dplyr::case_when(!!!cond_statements)) %>% eval_in_data(self)
+  vec_num <- rlang::expr(dplyr::case_when(!!!cond_statements)) %>% eval_in_data(self)
 
   self$dat_mod[[new_var]] <- haven::labelled(
-    x,
+    vec_num,
     labels = sum_var_vals_n_labs[-2] %>% dplyr::select(2, 1) %>% tibble::deframe(),
     label = new_lab
   )
@@ -375,10 +375,10 @@ apply_command.cmd_sumvar <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_dic <- function(x, self) {
-  orig_var <- x$args$orig_var
+apply_command.cmd_dic <- function(cdb, self) {
+  orig_var <- cdb$args$orig_var
   vec <- self$dat_mod[[orig_var]]
-  new_var <- x$args$new_var
+  new_var <- cdb$args$new_var
 
   varlab <- attr(vec, "label", exact = TRUE)
   vallabs <- attr(vec, "labels", exact = TRUE)
@@ -396,8 +396,8 @@ apply_command.cmd_dic <- function(x, self) {
 }
 
 #' @export
-apply_command.cmd_autorec <- function(x, self) {
-  var_name <- x$args$var
+apply_command.cmd_autorec <- function(cdb, self) {
+  var_name <- cdb$args$var
   vec <- self$dat_mod[[var_name]]
   x_labelled <- labelled::to_labelled(as.factor(vec))
   labelled::var_label(x_labelled) <- attr(vec, "label", exact = TRUE)
@@ -408,8 +408,8 @@ apply_command.cmd_autorec <- function(x, self) {
 
 # #STR2NUM
 #' @export
-apply_command.cmd_str_to_num <- function(x, self) {
-  var_name <- x$args$var
+apply_command.cmd_str_to_num <- function(cdb, self) {
+  var_name <- cdb$args$var
   var <- self$dat_mod[[var_name]]
   self$dat_mod[[var_name]] <- haven::labelled(
     var %>% tablab::strip_attributes() %>% as.numeric(),
