8f9d7696a1f6e84606e190bb760a678c41cfda10
Urs Wilke <urs.wilke@gmail.com>
2022-01-22 14:03:01 +0100

    fix: add possibility to omit ex_further_cond

--- R/verbatims.R
+++ R/verbatims.R
@@ -49,7 +49,7 @@ generate_verbatim_sheet_table <- function(mapping_file, sheet) {
       col_names = TRUE
     ) %>%
     tidyr::drop_na(.data$VariableOriginal) %>%
-    dplyr::select(.data$VariableOriginal:.data$`Tabellen-blatt`, .data$VariableZiel, .data$ex_further_cond) %>%
+    dplyr::select(.data$VariableOriginal:.data$`Tabellen-blatt`, .data$VariableZiel, dplyr::any_of("ex_further_cond")) %>%
     # HACK!!! TODO: replace with general regex
     dplyr::mutate(VariableZiel = un_OT_ize(.data$VariableZiel, .data$VariableOriginal) %>% un_OT_ize(.data$VariableOriginal) %>% un_OT_ize(.data$VariableOriginal)) %>%
     dplyr::relocate(q_id = .data$`Tabellen-blatt`)
@@ -224,7 +224,8 @@ translate_verbatim_line <- function(verbatim_type, verbatim_data) {
 
 generate_verbatim_assignment_table_raw <- function(l) {
   verbatim_types <- l %>% purrr::map_dbl(purrr::chuck, "meta", "EFA1MCG2MDG3")
-  ex_further_cond <- l %>% purrr::map_chr(purrr::chuck, "meta", "ex_further_cond")
+  # allow to not specify ex_further_cond in excel mapping file -> then write NA column
+  ex_further_cond <- l %>% purrr::map_chr(purrr::pluck, "meta", "ex_further_cond", .default = NA_character_)
   purrr::map2(verbatim_types, l, translate_verbatim_line) %>%
     purrr::map2(ex_further_cond, ~dplyr::mutate(.x, ex_further_cond = .y)) %>%
     dplyr::bind_rows(.id = "row")
