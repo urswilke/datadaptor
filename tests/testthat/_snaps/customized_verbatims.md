a130a325fe1da0f914661c6c36efa2a19adb5a39
Urs Wilke <urs.wilke@gmail.com>
2022-01-26 18:47:58 +0100

    add test for customized verbatims

---
+++ tests/testthat/_snaps/customized_verbatims.md
@@ -0,0 +1,59 @@
+# modified data print is reproduced
+
+    # A tibble: 10 x 7
+          id        q2           q3_1           q3_2           q3_3     q3_4    q3_5
+       <int> <dbl+lbl>      <dbl+lbl>      <dbl+lbl>      <dbl+lbl> <dbl+lb> <dbl+l>
+     1     1     4 [D] NA             NA              4 [rating: D] 4 [rati~ 0      
+     2     2     2 [B] NA              2 [rating: B] NA             0        0      
+     3     3     1 [A]  3 [rating: C] NA             NA             0        0      
+     4     4    NA     NA             NA             NA             0        0      
+     5     5     1 [A]  1 [rating: A] NA              1 [rating: A] 0        0      
+     6     6     1 [A]  3 [rating: C] NA              3 [rating: C] 0        0      
+     7     7     1 [A]  2 [rating: B]  2 [rating: B]  2 [rating: B] 0        0      
+     8     8     5 [E] NA             NA              2 [rating: B] 0        2 [rat~
+     9     9     1 [A]  4 [rating: D] NA              4 [rating: D] 0        0      
+    10    10     1 [A]  4 [rating: D] NA              4 [rating: D] 0        0      
+
+# variable labels are reproduced
+
+    # A tibble: 5 x 2
+      var   varlab
+      <chr> <chr> 
+    1 q3_1  A     
+    2 q3_2  B     
+    3 q3_3  C     
+    4 q3_4  D     
+    5 q3_5  E     
+
+# value labels are reproduced
+
+    # A tibble: 26 x 4
+       var      nv vallab    cv   
+       <chr> <dbl> <chr>     <chr>
+     1 q2       -2 FILTER    <NA> 
+     2 q2        1 A         <NA> 
+     3 q2        2 B         <NA> 
+     4 q2        3 C         <NA> 
+     5 q2        4 D         <NA> 
+     6 q2        5 E         <NA> 
+     7 q3_1      1 rating: A <NA> 
+     8 q3_1      2 rating: B <NA> 
+     9 q3_1      3 rating: C <NA> 
+    10 q3_1      4 rating: D <NA> 
+    11 q3_2      1 rating: A <NA> 
+    12 q3_2      2 rating: B <NA> 
+    13 q3_2      3 rating: C <NA> 
+    14 q3_2      4 rating: D <NA> 
+    15 q3_3      1 rating: A <NA> 
+    16 q3_3      2 rating: B <NA> 
+    17 q3_3      3 rating: C <NA> 
+    18 q3_3      4 rating: D <NA> 
+    19 q3_4      1 rating: A <NA> 
+    20 q3_4      2 rating: B <NA> 
+    21 q3_4      3 rating: C <NA> 
+    22 q3_4      4 rating: D <NA> 
+    23 q3_5      1 rating: A <NA> 
+    24 q3_5      2 rating: B <NA> 
+    25 q3_5      3 rating: C <NA> 
+    26 q3_5      4 rating: D <NA> 
+
---
+++ tests/testthat/test-customized_verbatims.R
@@ -0,0 +1,94 @@
+set.seed(2)
+q2 <- haven::labelled(c(3:1, NA_real_, rep(3, 6)), labels = purrr::set_names(1:3, c(LETTERS[1:2], "Other, namely:")))
+q3_1 <- haven::labelled(c(NA, NA, 3, NA, NA, 1, rep(NA, 4)), labels = purrr::set_names(1:4, c(LETTERS[1:4])))
+q3_2 <- haven::labelled(c(NA, 2, rep(NA, 4), 2, rep(NA, 3)), labels = purrr::set_names(1:4, c(LETTERS[1:4])))
+q3_3 <- haven::labelled(c(4, NA, NA, NA, sample(1:4, 6, replace = TRUE)), labels = purrr::set_names(1:4, paste("rating:", LETTERS[1:4])))
+
+
+dat <- tibble::tibble(
+  id = 1:10,
+  q2,
+  q3_1,
+  q3_2,
+  q3_3
+)
+meta_mdg_custom <- tibble::tibble(
+  q_id = "mdg",
+  VariableOriginal = "var_containing_open_answer_strings",
+  EFA1MCG2MDG3 = "mdg_custom",
+  VariableZiel = "q3_{nn}",
+  ex_further_cond = 'q2 == 3',
+  ex_assign = "q3_3"
+)
+meta_efa <- tibble::tibble(
+  q_id = "mdg",
+  VariableOriginal = "var_containing_open_answer_strings",
+  EFA1MCG2MDG3 = "1",
+  VariableZiel = "q2",
+)
+
+assignments <- tibble::tibble(
+  ID = 1:10,
+  Antwort = LETTERS[1:10],
+  `Zuord 1` = c(4, NA, NA, NA, sample(c(1, 2, 4, 5), 6, TRUE)),
+)
+
+labs <- tibble::tibble(
+  Code = 1:5,
+  lab = LETTERS[1:5]
+)
+
+name <- "verba_tab_for_variable_map"
+make_cdb_raw <- function(name, meta, assignments, labs) {
+  cdb_raw <- list(
+    name = name,
+    meta = meta,
+    assignments = assignments,
+    labs = list()
+  )
+  cdb_raw$labs$name = labs
+  cdb_raw
+}
+
+# It's important to execute mdg_custom before efa, because
+# efa will change the value of of q2 (serving for ex_further_cond of mdg_custom):
+cdbs_raw <- list(meta_mdg_custom, meta_efa) %>%
+  purrr::map(~make_cdb_raw(name = name, meta = .x, assignments = assignments, labs = labs))
+
+m <- Mapping$new(dat, na_to_filter = FALSE)
+m$params$id_var <- "id"
+m$cmd$sheet_data_raw$Verbatims <- cdbs_raw
+m$cmd$sheet_cats <- tibble::tibble(sheet = "Verbatims", sheet_type = "Verbatims")
+
+m$cmd$df_cmd_raw <- datenanpassr:::gen_command_table_raw(m)
+m$cmd$command_blocks_raw <- datenanpassr:::gen_command_blocks_raw(m)
+# m$cmd$command_blocks_raw <- purrr::map(
+#   m$cmd$command_blocks_raw,
+#   ~ {
+#     class(.x) <- c("cmd_new_verba", class(.x))
+#     .x
+#   }
+# )
+
+m$cmd$command_blocks <- datenanpassr:::command_blocks(m)
+m$cmd_tbl <- datenanpassr:::gen_command_table(m)
+
+m$modify_data()
+test_that("modified data print is reproduced", {
+  testthat::expect_snapshot_output(
+    m$dat_mod
+  )
+})
+test_that("variable labels are reproduced", {
+  testthat::expect_snapshot_output(
+    m$dat_mod %>% tablab::tab_varlabs()
+  )
+})
+test_that("value labels are reproduced", {
+  testthat::expect_snapshot_output(
+    m$dat_mod %>% tablab::tab_vallabs() %>% print(n=111)
+  )
+})
+
+
+
