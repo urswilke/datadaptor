3f93bcc6007c9977bcc826d56ff280e7a41d950a
Urs Wilke <urs.wilke@gmail.com>
2022-01-31 16:55:06 +0100

    edit comments

--- R/utils.R
+++ R/utils.R
@@ -84,11 +84,14 @@ curliply_headers <- function(df) {
 
 
 chop_out_curly_parts <- function(x) {
-  # split string into list of substrings at "{" and "}"
-  # Explanation regex:
+  # split string x into list of substrings at "{" and "}"
+  # Explanation:
+  # regex tries to cut out the separator.
+  # The positive look-aheads & -behinds, will match but keep the separator.
   # split at curly bracket (keeping the brackets)"\\{" if not at the beginning, or
   # split at curly bracket "\\}" if not at the end of the string.
-  # (positive/negative) look-aheads & -behinds:
+  # pseudo code:
+  # (not a beginning of x)(there is a curly "{"}) OR (not at the end)(there is a "}")
   stringr::str_split(x, "(?<!^)(?=\\{)|(?<=\\})(?!$)")
 }
 
