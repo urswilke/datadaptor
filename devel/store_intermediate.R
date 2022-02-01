55fb9fdf1c725688f2d2314f3e4dddfe5913193d
Urs Wilke <urs.wilke@gmail.com>
2022-02-01 23:16:28 +0100

    update

--- devel/store_intermediate.R
+++ devel/store_intermediate.R
@@ -1,28 +1,30 @@
 library(datenanpassr)
 library(tidyverse)
 mapping_file <- system.file("extdata", "mapping.xlsx", package = "datenanpassr")
-accumulate_command_blocks <- function(m, gen_intermediate_mapping = gen_intermediate_mapping_change) {
+accumulate_command_blocks <- function(m) {
+  # generate a list of nrow(`m$cmd_tbl`) command_blocks elements, each
+  # containing just one line of `m$cmd_tbl`:
+  single_cdbs <- purrr::map(
+    1:nrow(m0$cmd_tbl),
+    ~m0$cmd_tbl %>% dplyr::slice(.x) %>% dplyr::pull(command_blocks)
+  )
 
-
-  single_cdbs <- purrr::map(1:nrow(m$cmd_tbl), ~m$cmd_tbl %>% dplyr::slice(.x) %>% dplyr::pull(command_blocks))
-
-    l <- purrr::map(single_cdbs, gen_intermediate_mapping)
+  # apply each singe row command_blocks to the result of the previous mapping,
+  # generating a list of Mapping objects (like purrr::accumulate but for R6):
+  gen_intermediate_mapping <- function(cdb, mm = m0) {
+    mm$modify_data(reset = FALSE, command_blocks = cdb)$clone(deep = TRUE)
+  }
+  l <- purrr::map(single_cdbs, gen_intermediate_mapping)
+  # Extract R6 field of all Mapping objects in list:
   res <- purrr::map(l, "dat_mod")
-  # https://r6.r-lib.org/articles/Introduction.html#finalizers
-  rm(l); gc()
-  res
-}
 
-gen_intermediate_mapping_keep <- function(cdb, m = m0) {
-  m$clone(deep = TRUE)$modify_data(reset = FALSE, command_blocks = cdb)
-}
-gen_intermediate_mapping_change <- function(cdb, m = m0) {
-  m$modify_data(reset = FALSE, command_blocks = cdb)$clone(deep = TRUE)
+  # https://r6.r-lib.org/articles/Introduction.html#finalizers
+  rm(l, m0); gc()
+  res
 }
 
 m <- Mapping$new(fake_survey, mapping_file)
 m0 <- m$clone(deep = TRUE)
 m0$dat_mod <- m0$dat
-# doesn't work for cmd_rename ??? :
-# l_dat <- accumulate_command_blocks(m0, gen_intermediate_mapping_keep)
+
 l_dat <- accumulate_command_blocks(m0)
