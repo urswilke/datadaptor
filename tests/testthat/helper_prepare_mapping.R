8f32c5e52ceb086277142722588a0d4acf1afc54
Urs Wilke <urs.wilke@gmail.com>
2022-02-11 11:17:50 +0100

    don't render rmarkdown locally, because it takes several seconds...

--- tests/testthat/helper_prepare_mapping.R
+++ tests/testthat/helper_prepare_mapping.R
@@ -1,4 +1,5 @@
 library(dplyr, warn.conflicts = FALSE)
+library(stringr)
 mapping_file <- system.file("extdata", "mapping.xlsx", package = "datenanpassr")
 spss_file <- system.file("extdata", "fake_survey.sav", package = "datenanpassr")
 
--- tests/testthat/test-save_mapping.R
+++ tests/testthat/test-save_mapping.R
@@ -1,18 +1,18 @@
 mapping_file <- "excel/test_save.xlsx"
 expect_message(expect_message(m <- Mapping$new(dat = tibble(a = c(1:3, NA)), mapping_file = mapping_file)))
 path <- m$params$save_path
-save_file_types <- c("sav", "dta", "xlsx")
+save_file_types <- c("sav", "dta", "xlsx", "Rmd")
 filenames <- "myfilename"
 output_files <- paste0(path, "/", filenames, ".", save_file_types)
 test_that("Mapping$save() seems to work", {
 
   withr::with_file(output_files, {
     m$modify_data()
-    m$save(name = filenames, filetype = save_file_types)
+    m$save(name = filenames, filetype = save_file_types[1:3])
     spss_output_vec  <- haven::read_sav(output_files[1])$a
     stata_output_vec <- haven::read_dta(output_files[2])$a
     excel_output_vec <- readxl::read_excel(output_files[3])$a
-    vallabs_sav <- attr(stata_output_vec, "labels")
+    vallabs_sav <- attr(spss_output_vec, "labels")
     vallabs_dta <- attr(stata_output_vec, "labels")
     expect_true(
       unique(c(names(vallabs_dta), names(vallabs_sav))) ==  "FILTER"
@@ -28,3 +28,19 @@ test_that("Mapping$save() seems to work", {
 
   })
 })
+
+# I don't want the testthat output cluttered. therefore `if` and not `skip`...:
+if (testthat:::on_ci()) {
+  test_that("Mapping$save() seems to work for Rmd", {
+    # skip_if_not(testthat:::on_ci())
+
+    withr::with_file(output_files, {
+      m$modify_data()
+      m$save(name = filenames, filetype = save_file_types[4])
+      expect_true(file.exists(output_files[4]))
+      expect_true(file.exists(str_replace(output_files[4], "Rmd$", "html")))
+
+    })
+  })
+
+}
