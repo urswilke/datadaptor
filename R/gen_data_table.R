899d6fd0ed52261c0b8f80d2020f6d21bf067419
Urs Wilke <urs.wilke@gmail.com>
2022-10-26 02:11:10 +0200

    next version with wrong ordering of nv column

--- R/gen_data_table.R
+++ R/gen_data_table.R
@@ -1,24 +1,33 @@
 #' Generate data counts table
 #'
 #' @param df dataframe
+#' @param values_drop_na remove missing values? (passed to `tidyr::pivot_longer()`.)
 #'
 #' @return Counts and labels data frame
 #' @export
 #'
 #' @examples
 #' gen_data_table(fake_survey)
-gen_data_table <- function(df) {
-  count1 <- df %>%
-    purrr::map(table) %>%
-    purrr::map(as.data.frame)
-  res1 <- dplyr::bind_rows(count1, .id="var")
-  colnames(res1)[colnames(res1)=="Var1"] <- "nv"
+gen_data_table <- function(df, values_drop_na = TRUE) {
+  res1 <- df |>
+    tidyr::pivot_longer(
+      dplyr::everything(),
+      # in order to merge string & numeric variables into one column:
+      values_transform = as.character,
+      # in order to keep variable order:
+      names_transform = forcats::as_factor,
+      # remove missing values:
+      values_drop_na = values_drop_na,
+      names_to = "var",
+      values_to = "nv"
+    ) |>
+    dplyr::count(.data$var, .data$nv, name = "Freq")
 
   var1 <- gen_var_table(df) %>%
-    dplyr::select(dplyr::all_of(c("var","type", "varlab")))
+    dplyr::select(c("var","type", "varlab"))
 
   label1 <- tablab::tab_vallabs(df) %>%
-    dplyr::select(dplyr::all_of(c("var","nv", "vallab")))
+    dplyr::select(c("var","nv", "vallab"))
 
   res1 <- merge(res1, label1, by=c("var", "nv"), all=TRUE)
   merge(res1, var1, by="var")
