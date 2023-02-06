4618aa51733cfa950defd2e7d4a3bf61f867fcce
urswilke <urs.wilke@gmail.com>
2023-02-06 20:20:51 +0100

    access widths via attributes

--- devel/print_varlabs_in_tibble.R
+++ devel/print_varlabs_in_tibble.R
@@ -3,14 +3,9 @@ ctl_new_pillar.labelled_tbl <- function(controller, x, ..., title = NULL) {
   out <- NextMethod()
   varlab <- attr(x, "label", exact = TRUE)
 
-  width <- pillar::get_max_extent(x)
-  if (length(width) == 0 | width == 0) {
-    width <- 1
-  }
-  vallabs_widths <- out[["data"]][[1]][["lbl"]][["wid_full"]]
-  vallabs_width <- ifelse(is.null(vallabs_widths[1]), 0, max(vallabs_widths))
+  data_width <- attr(out[["data"]], "width")
   type_width <- attr(out[["type"]], "width")
-  width <- max(width + vallabs_width, type_width)
+  width <- max(data_width, type_width)
   if (is.null(varlab)) {
     varlab <- rep("-", width) |> paste(collapse = "")
   }
