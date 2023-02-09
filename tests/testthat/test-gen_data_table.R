4398fdddb08645dec20886df4a205e452addd7ef
urswilke <urs.wilke@gmail.com>
2023-02-09 19:29:54 +0100

    use dataframe from SPSS file instead of package dataset

--- tests/testthat/test-gen_data_table.R
+++ tests/testthat/test-gen_data_table.R
@@ -1,6 +1,6 @@
 test_that("gen_data_table() works", {
   testthat::expect_snapshot_output({
-    gen_data_table(fake_survey) |> dplyr::as_tibble() |> print(n=1111)
+    gen_data_table(dat) |> dplyr::as_tibble() |> print(n=1111)
   })
 })
 
