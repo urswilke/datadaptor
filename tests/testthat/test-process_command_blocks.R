41c838805a16672e9ee44ea87ebac183bc85cd92
urswilke <urs.wilke@gmail.com>
2023-02-09 10:51:08 +0100

    this test didn't make sense

--- tests/testthat/test-process_command_blocks.R
+++ tests/testthat/test-process_command_blocks.R
@@ -4,9 +4,6 @@ test_that("parameters (except those containing filepaths) were built correctly",
     mapping_s3$params[!names(mapping_s3$params) %in% c("expr_eval_env", "mapping_file", "save_path")])
 })
 
-cb_err <- mapping_s3$cmd$df_cmd_raw[10,]
-cb_err$action <- "uillu"
-testthat::expect_error(cbs_err <- cb_err |> command_block() |> parse_command_args() |> list(.) |> datenanpassr:::new_command_blocks(subclass = "unsafe"))
 
 mapping_raw_list <- list(Variables = tibble(
   var = "q1",
