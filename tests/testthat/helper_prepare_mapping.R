aa7c91cf83f98b3552de36b22ac65c0696c3da73
urswilke <urs.wilke@gmail.com>
2023-02-10 11:18:26 +0100

    add comments for interactive use

--- tests/testthat/helper_prepare_mapping.R
+++ tests/testthat/helper_prepare_mapping.R
@@ -1,6 +1,8 @@
 library(dplyr, warn.conflicts = FALSE)
 library(stringr)
+# mapping_file <- "tests/testthat/excel/mapping_old.xlsx"
 mapping_file <- "excel/mapping_old.xlsx"
+# spss_file <- "tests/testthat/spss/fake_survey.sav"
 spss_file <- "spss/fake_survey.sav"
 dat <- haven::read_sav(spss_file)
 
