16232b191e210bb56cf36a4d9ce5f5ea8ed6e95a
Urs Wilke <urs.wilke@gmail.com>
2023-08-02 21:31:12 +0200

    change: don't add function index suffix when passing a function (not an unnamed list of length 1) as the .fns arg of across() (this allows to modify the input variables without creating a renamed copy)

--- R/command_block_methods.R
+++ R/command_block_methods.R
@@ -106,13 +106,18 @@ apply_command.cmd_select <- function(cdb, mapping, exs, ...) {
 #'
 #' @export
 apply_command.cmd_across <- function(cdb, mapping, exs, ex_fun, exs_fns_names, ex_names, ...) {
-  exs_ex <- parse_exprs(exs)
+  exs_ex <- parse_expr(exs)
   ex_fun <- ex_fun |> map(parse_expr) |> map(eval_tidy)
   if (!is.null(exs_fns_names)) {
     stopifnot(length(ex_fun) == length(exs_fns_names))
     names(ex_fun) <- exs_fns_names
   }
-  mapping$dat_mod <- mapping$dat_mod |> mutate(across(!!!exs_ex, ex_fun, .names = ex_names))
+  # in order to prevent the function name suffix when passing an unnamed list to
+  # across()
+  if (length(ex_fun) == 1 & is.null(exs_fns_names)) {
+    ex_fun <- ex_fun[[1]]
+  }
+  mapping$dat_mod <- mapping$dat_mod |> mutate(across(!!exs_ex, ex_fun, .names = ex_names))
 }
 #' @describeIn apply_command
 #'
