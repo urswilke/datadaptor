df98034de10cad2e6c27deedb8809f75cddbb90f
Urs Wilke <urs.wilke@gmail.com>
2024-10-04 03:11:12 +0200

    - rename proj_path to new_proj_path - add documentation - pass ... to starter::create_project() - open Rstudio via browseURL() via arg open_rstudio before setting up renv

--- R/create_project.R
+++ R/create_project.R
@@ -1,7 +1,8 @@
 #' Create Mapping directory structure from template folder
 #'
-#' @param proj_path The path of your new mapping project.
+#' @param new_proj_path The path of your new mapping project.
 #' @param proj_template_path The template folder path; the first word should be the project leader initials.
+#' @param mapping_subpath The subpath of the mapping file within `proj_template_path`.
 #' @param proj_folder_name The project folder name (defaults to the path name `proj_template_path` without parent directories).
 #' @param proj_name The project folder name with the project leader initials removed.
 #' @param proj_leader First word in `proj_template_path`.
@@ -9,18 +10,19 @@
 #' @param use_renv Whether to use renv.
 #' @param use_git Whether to setup git.
 #' @param open_mapping Whether to open the Excel mapping file.
+#' @param ... Arguments passed to `starter::create_project()`.
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' proj_path <- "PATH/WHERE/TO/CREATE/THE/NEW/MAPPING/UW My Project Name"
-#' create_mapping_project(proj_path)
+#' new_proj_path <- "PATH/WHERE/TO/CREATE/THE/NEW/MAPPING/UW My Project Name"
+#' create_mapping_project(new_proj_path)
 #' }
-create_mapping_project <- function(proj_path,
+create_mapping_project <- function(new_proj_path,
                                    proj_template_path = "K:/Tools/TableBook/202308/Template Folder/",
                                    mapping_subpath = "Syntax/Mapping.xlsm",
-                                   proj_folder_name = proj_path |>
+                                   proj_folder_name = new_proj_path |>
                                      stringr::str_remove("/$") |>
                                      stringr::str_remove(".*/"),
                                    proj_name = proj_folder_name |> stringr::word(2, -1),
@@ -28,11 +30,13 @@ create_mapping_project <- function(proj_path,
                                    use_rproj = TRUE,
                                    use_renv = TRUE,
                                    use_git = TRUE,
-                                   open_mapping = TRUE
+                                   open_mapping = TRUE,
+                                   open_rstudio = TRUE,
+                                   ...
                                    ) {
-  mapping_template_path <- file.path(proj_path, mapping_subpath)
+  mapping_template_path <- file.path(new_proj_path, mapping_subpath)
 
-  fs::dir_copy(proj_template_path, proj_path)
+  fs::dir_copy(proj_template_path, new_proj_path)
 
   mapping_proj_path <- stringr::str_replace(mapping_template_path, "\\.xlsm", paste0(" ", proj_name, ".xlsm"))
   fs::file_move(mapping_template_path, mapping_proj_path)
@@ -62,15 +66,19 @@ create_mapping_project <- function(proj_path,
       ))
     )
 
-    starter::create_project(proj_path,
+    starter::create_project(new_proj_path,
                             template = starter_template,
                             git = use_git,
                             renv = use_renv,
-                            open = TRUE)
+                            open = FALSE,
+                            ...)
+    if (open_rstudio) {
+      browseURL(file.path(new_proj_path, "R", proj_name, ".Rproj"))
+    }
     if (use_renv) {
-      renv::activate(proj_path)
-      renv::snapshot(proj_path, packages = c("datenanpassr", "crosstabser"), prompt = FALSE, force = TRUE)
-      renv::deactivate(proj_path)
+      renv::activate(new_proj_path)
+      renv::snapshot(new_proj_path, packages = c("datenanpassr", "crosstabser"), prompt = FALSE, force = TRUE)
+      renv::deactivate(new_proj_path)
     }
     cli::cli_alert_success("In your new Rstudio project type `usethis::use_git()` to commit the files.")
 
